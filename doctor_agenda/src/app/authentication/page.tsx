ddf249325fc3dc00e8c3c5b8b0f0c4546c7167a6
AlvesBelem <marcelocartoes.pa@gmail.com>
2025-05-27 16:51:55 -0300

    feat: add form sign in and sign up form

---
+++ doctor_agenda/src/app/authentication/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import SignInForm from "./components/sign-in-form";
+import SignUpForm from "./components/sign-up-form";
+
+// const loginSchema = z.object({
+//   email: z.string().trim().email({ message: "Email inválido" }),
+//   password: z
+//     .string()
+//     .min(8, { message: "Senha deve ter pelo menos 8 caracteres" }),
+// });
+
+export default function AuthenticationPage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <h1 className="m-4 text-center text-2xl font-bold">Doctor Agenda</h1>
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Faça seu Login</TabsTrigger>
+          <TabsTrigger value="register">Criar sua conta</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <SignInForm />
+        </TabsContent>
+        <TabsContent value="register">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
